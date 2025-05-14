2f575bceee83d1a6687d6c64654b32d6a5388420
nayaracalenzo <queiroz.14@hotmail.com>
2025-05-14 17:00:07 -0300

    adicionando ant design

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import { Alert } from 'antd';
+import { Link } from 'react-router-dom';
 
 const NotFound = () => {
   return (
-    <div>Página NotFound</div>
+    <>
+    <section className='flex flex-col gap-10 px-5'>
+        <Alert
+        message="Página não encontrada!"
+        description="Desculpe, a página que você procura não existe ou não foi encontrada!"
+        type="error"
+        showIcon
+        />
+        <p>Não se preocupe! Você pode voltar para nossa <span> <Link to='/' className='hover:underline text-blue-500'>página inicial</Link></span></p>
+    </section>
+    </>
   )
 }
 
