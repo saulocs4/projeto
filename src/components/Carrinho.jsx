b87a006d451855a1e46a6ba0ad64d76945b9dead
nayaracalenzo <queiroz.14@hotmail.com>
2025-05-15 17:06:08 -0300

    produtos com useState

--- src/components/Carrinho.jsx
+++ src/components/Carrinho.jsx
@@ -29,6 +29,10 @@ const produtos = [
 
 const Carrinho = () => {
     const [count, setCount] = useState(0)
+
+    function addToCart() {
+        setCount((count) => count + 1)
+    }
   return (
     <>
     <section className='bg-white p-8'>
@@ -68,19 +72,19 @@ const Carrinho = () => {
             </tbody>
         </table>
     </section>
-    <section>
-        <h1>Produtos Relacionados</h1>
+    <section className='ml-8'>
+        <h1 className='font-bold text-2xl mb-5'>Produtos Relacionados</h1>
         <ul className='flex gap-20'>
             {produtos.map((item)=> (
                 <li className='border border-[#ccc] p-2'>
                     <div className='bg-[#E2E3FF] h-28 w-32 flex items-center justify-center rounded-sm '>
-                            <img src={item.img} alt={item.produto} />
+                        <img src={item.img} alt={item.produto} />
                     </div>
                     <div>
                         <h2>{item.categoria}</h2>
                         <h2 className='text-xs'>{item.produto}</h2>
                         <p>{item.preco}</p>
-                        <button className='bg-green-700 w-[80%] rounded-md'>Add</button>
+                        <button onClick={() => addToCart()} className='bg-green-700  w-[100%] rounded-md'>Add</button>
                     </div>
                 </li>
             ))}
