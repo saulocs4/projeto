ec9ce35c4db9262370e2a2a5fa37566b5e766510
nayaracalenzo <queiroz.14@hotmail.com>
2025-05-16 14:14:51 -0300

    iniciando context API

---
+++ src/context/CartContext.jsx
@@ -0,0 +1,13 @@
+import { createContext } from "react"
+
+const CartContext = createContext()
+
+const CartProvider = ({children}) => {
+
+  return (
+    <>
+    </>
+  )
+}
+
+export {CartContext, CartProvider}
---
+++ src/context/ProductsContext.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductsContext = () => {
+  return (
+    <div>ProductsContext</div>
+  )
+}
+
+export default ProductsContext
