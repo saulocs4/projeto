3fd8e37921f5508686bdec441e862346a39d5c5a
nayaracalenzo <queiroz.14@hotmail.com>
2025-05-16 17:17:26 -0300

    react-icons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,22 +4,14 @@ import { FaBars } from 'react-icons/fa'
 
 const Header = () => {
   return (
-    <header className="bg-[#242e56]">
-    <h1 className="font-bold text-2xl">Livraria GT</h1>
-    <form className="form-header">
-        <input type="text" id="searchInput" className="bg-white text-black pl-3"/>
-        <button id="searchButton" className="bg-white h-10 flex"><i className="text-black fa-solid fa-magnifying-glass"></i></button>
-    </form>
-    <nav className="nav-header">
-        <ul className="lista-links">
-            <li>Home</li>
-            <li>Categorias</li>
-        </ul>
-        <ul>
+    <header className="bg-[#d0d0d2] p-10 flex justify-between items-center">
+      <h1 className='font-bold text-5xl text-pink-600'>Header</h1>
+    <nav>
+        <ul className='flex gap-2'>
             <li>
               <FaCartShopping/>
             </li>
-            <li className="lista-icone">
+            <li>
               <FaBars/>
             </li>
         </ul>
